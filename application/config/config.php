1b063e85c982192707e9e990cc5dc5991f0c3384
Pablo <pablo.gallardo22@gmail.com>
2017-08-09 10:35:13 -0300

    cambiado config base_url para poder compartir

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ $config['precio_cambio'] = 20.45;
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.11/cava/';
+$config['base_url'] = "http://" . $_SERVER['SERVER_NAME'] . "/cava/";
 
 /*
 |--------------------------------------------------------------------------
