602b0cb89fb7034e67b0c73922dc05d688d66cf8
Pablo <pablo.gallardo22@gmail.com>
2017-08-29 21:52:58 -0300

    Correcciones menores en pantalla INFORMES

--- application/views/informe_view.php
+++ application/views/informe_view.php
@@ -1,43 +1,45 @@
-			<div class="content-box-large">
-  				<div class="panel-heading">
+			<div class="col-md-12 panel-info">
+  				<div class="content-box-header panel-heading">
 					<div class="panel-title">Informe de Pagos</div>
 				</div>
-  				<div class="panel-body">
-  					<div class="table-responsive">
-  						<table class="table">
-			              <thead>
-			                <tr>
-			                  <th>N° Ticket</th>
-			                  <th>Fecha</th>
-			                  <th>Nombre</th>
-			                  <th>Apellido</th>
-			                  <th>Tipo Pago</th>
-			                  <th>Ar$</th>
-			                </tr>
-			              </thead>
-			              <?php $total = 0; ?>
-			              <tbody>
-			                <?php foreach ($tickets as $ticket):?>
-							<tr class="odd gradeX">
-								<td><?=$ticket->id_ticket?></td>
-								<td><?=$ticket->fecha_ticket?></td>
-								<td><?=$ticket->nombre?></td>
-								<td><?=$ticket->apellido?></td>
-								<td><?=$ticket->tipo?></td>	
-								<td><?=$ticket->pesos?></td>								
-							</tr>
-							<?php $total = $total + $ticket->pesos; ?>
-							<?php endforeach; ?>
-							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td><b>TOTAL:</b></td>
-								<td><b><?= $total ?></b></td>
-							</tr>
-			              </tbody>
-			            </table>
-  					</div>
+  				<div class="content-box-large box-with-header">
+  					<div id="rootwizard">
+	  					<div class="table-responsive">
+	  						<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered">
+				              <thead>
+				                <tr>
+				                  <th>N° Ticket</th>
+				                  <th>Fecha</th>
+				                  <th>Nombre</th>
+				                  <th>Apellido</th>
+				                  <th>Tipo Pago</th>
+				                  <th>Ar$</th>
+				                </tr>
+				              </thead>
+				              <?php $total = 0; ?>
+				              <tbody>
+				                <?php foreach ($tickets as $ticket):?>
+								<tr class="odd gradeX">
+									<td><?=$ticket->id_ticket?></td>
+									<td><?=$ticket->fecha_ticket?></td>
+									<td><?=$ticket->nombre?></td>
+									<td><?=$ticket->apellido?></td>
+									<td><?=$ticket->tipo?></td>	
+									<td><?='$ '.$ticket->pesos?></td>								
+								</tr>
+								<?php $total = $total + $ticket->pesos; ?>
+								<?php endforeach; ?>
+								<tr>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td><b>TOTAL:</b></td>
+									<td><b><?= '$ '.$total ?></b></td>
+								</tr>
+				              </tbody>
+				            </table>
+	  					</div>
+	  				</div>
   				</div>
   			</div>
